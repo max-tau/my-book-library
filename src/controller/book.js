ab87c920bebc4c5cdf91cd8ea079f7d9783cf29f
Max Tauroza <maxtauroza@gmail.com>
2023-06-01 16:53:10 +0100

    add functionality to pass all book tests

--- src/controller/book.js
+++ src/controller/book.js
@@ -1,1 +1,43 @@
 const { Book } = require('../models');
+
+exports.create = async (req, res) => {
+  const newBook = await Book.create(req.body);
+
+  res.status(201).json(newBook);
+};
+
+exports.findAll = async (_, res) => {
+  const books = await Book.findAll();
+
+  res.status(200).json(books);
+};
+
+exports.findById = async (req, res) => {
+  const { id } = req.params;
+  const book = await Book.findByPk(id);
+
+  if (!book) {
+    res.status(404).json({ error: 'The book could not be found.' });
+  }
+  res.status(200).json(book);
+};
+
+exports.updateGenre = async (req, res) => {
+  const { id } = req.params;
+  const [book] = await Book.update(req.body, { where: { id } });
+
+  if (!book) {
+    res.status(404).json({ error: 'The book could not be found.' });
+  }
+  res.status(200).json(book);
+};
+
+exports.delete = async (req, res) => {
+  const { id } = req.params;
+  const deletedBook = await Book.destroy({ where: { id } });
+
+  if (!deletedBook) {
+    res.status(404).json({ error: 'The book could not be found.' });
+  }
+  res.status(204).json(deletedBook);
+};
--- src/route/book.js
+++ src/route/book.js
@@ -3,4 +3,11 @@ const bookController = require('../controller/book');
 
 const bookRouter = express.Router();
 
+bookRouter
+  .post('/', bookController.create)
+  .get('/', bookController.findAll)
+  .get('/:id', bookController.findById)
+  .patch('/:id', bookController.updateGenre)
+  .delete('/:id', bookController.delete);
+
 module.exports = bookRouter;
